18f5b89851c54a867faad4c87b8d13390c8c528d
petrap12 <petrap12@ru.is>
2015-11-04 11:24:38 +0000

    Price slider added

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -6,21 +6,50 @@
 
 var hamburgerMenu = document.getElementById('hamburgerMenu');
 
-/* Er þetta notað? */
-//$(function () {
-//    var slider = $("#slider-range");
-//    $("#slider-range").slider({
-//        range: true,
-//        min: 0,
-//        max: 500,
-//        values: [75, 300],
-//        slide: function (event, ui) {
-//            $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
-//        }
-//    });
-//    $("#amount").val("$" + $("#slider-range").slider("values", 0) +
-//        " - $" + $("#slider-range").slider("values", 1));
-//});
+
+function getVals(){
+    // Get slider values
+    var parent = this.parentNode;
+    var slides = parent.getElementsByTagName("input");
+    var slide1 = parseFloat( slides[0].value );
+    var slide2 = parseFloat( slides[1].value );
+    // Neither slider will clip the other, so make sure we determine which is larger
+    if( slide1 > slide2 ){ var tmp = slide2; slide2 = slide1; slide1 = tmp; }
+
+    var displayElement = parent.getElementsByClassName("rangeValues")[0];
+    displayElement.innerHTML = slide1 + " - " + slide2;
+}
+
+window.onload = function(){
+    // Initialize Sliders
+    var sliderSections = document.getElementsByClassName("range-slider");
+    for( var x = 0; x < sliderSections.length; x++ ){
+        var sliders = sliderSections[x].getElementsByTagName("input");
+        for( var y = 0; y < sliders.length; y++ ){
+            if( sliders[y].type ==="range" ){
+                sliders[y].oninput = getVals;
+                // Manually trigger event first time to display values
+                sliders[y].oninput();
+            }
+        }
+    }
+}
+
+/*$(function () {
+    alert("in func");
+    var slider = $("#slider-range");
+    $("#slider-range").slider({
+        range: true,
+        min: 0,
+        max: 500,
+        values: [75, 300],
+        slide: function (event, ui) {
+            $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1]);
+        }
+    });
+    $("#amount").val("$" + $("#slider-range").slider("values", 0) +
+        " - $" + $("#slider-range").slider("values", 1));
+});*/
 
 function hamburgerShow() {
     let menu =  $('#hamburgerMenu');
@@ -33,8 +62,4 @@ function hamburgerShow() {
         menu.addClass("offScreen");
         menu.removeClass("onScreen");
     }
-}
-
-function showValue(newValue) {
-    document.getElementById("price").innerHTML = newValue;
 }
